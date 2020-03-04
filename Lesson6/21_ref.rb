ac8fc54b5a5225ab034317393ad7a4b62fcb2c67
Callie Buruchara <callieburuchara@gmail.com>
2020-03-04 23:04:19 +0300

    Finished basic 21 game

--- Lesson6/21_ref.rb
+++ Lesson6/21_ref.rb
@@ -1,24 +1,6 @@
-DECK = ["Ace", "Hearts"], ["Ace", "Diamonds"],
-          ["Ace", "Clubs"], ["Ace", "Spades"],
-          ["2", "Hearts"], ["2", "Diamonds"], ["2", "Clubs"],
-          ["2", "Spades"], ["3", "Hearts"], ["3", "Diamonds"],
-          ["3", "Clubs"], ["3", "Spades"], ["4", "Hearts"],
-          ["4", "Diamonds"], ["4", "Clubs"], ["4", "Spades"],
-          ["5", "Hearts"], ["5", "Diamonds"], ["5", "Clubs"],
-          ["5", "Spades"], ["6", "Hearts"], ["6", "Diamonds"],
-          ["6", "Clubs"], ["6", "Spades"], ["7", "Hearts"],
-          ["7", "Diamonds"], ["7", "Clubs"], ["7", "Spades"],
-          ["8", "Hearts"], ["8", "Diamonds"], ["8", "Clubs"],
-          ["8", "Spades"], ["9", "Hearts"], ["9", "Diamonds"],
-          ["9", "Clubs"], ["9", "Spades"], ["10", "Hearts"],
-          ["10", "Diamonds"], ["10", "Clubs"],
-          ["10", "Spades"], ["Jack", "Hearts"],
-          ["Jack", "Diamonds"], ["Jack", "Clubs"],
-          ["Jack", "Spades"], ["Queen", "Hearts"],
-          ["Queen", "Diamonds"], ["Queen", "Clubs"],
-          ["Queen", "Spades"], ["King", "Hearts"],
-          ["King", "Diamonds"], ["King", "Clubs"],
-          ["King", "Spades"]
+SUITS = ['H', 'D', 'S', 'C']
+VALUES = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+DECK = VALUES.product(SUITS).shuffle
 
 def prompt(words)
   puts "♣♦  #{words}  ♥♠"
@@ -38,22 +20,20 @@ end
 players_cards = []
 dealers_cards = []
 
-def initialize_deck(player, dealer)
+def initialize_cards(player, dealer)
   player << DECK.sample
   player << DECK.sample
   dealer << DECK.sample
   dealer << DECK.sample
-  # did it this way, lest it's an overly nested array 
-  # if I use sample(2)
 end
 
-def player_current_cards(cards)
-  cards.map {|card| card[0]}
+def current_cards(cards)
+  cards.map { |card| card[0] }
 end
 
 def display_current_cards(player, dealer)
   prompt "Dealer has: #{dealer[0][0]} and unknown card"
-  prompt "You have: " + joinor(player_current_cards(player))
+  prompt "You have: " + joinor(current_cards(player))
 end
 
 def hit!(cards)
@@ -61,17 +41,17 @@ def hit!(cards)
 end
 
 def total(cards) # fix this here
-  values = cards.map {|card| card[0]}
+  values = cards.map { |card| card[0] }
 
   sum = 0
   values.each do |value|
-    if value.start_with?("A")
-      sum += 11
-    elsif value.to_i == 0
-      sum += 10
-    else
-      sum += value.to_i
-    end
+    sum += if value.start_with?("A")
+             11
+           elsif value.to_i == 0
+             10
+           else
+             value.to_i
+           end
   end
 
   values.select { |value| value.start_with?("A") }.count.times do
@@ -84,6 +64,7 @@ def busted?(cards)
   total(cards) > 21
 end
 
+# rubocop:disable Metrics/MethodLength
 def player_turn(player, dealer)
   answer = nil
   loop do
@@ -101,19 +82,19 @@ def player_turn(player, dealer)
     sleep 1
     "busted"
   else
-    prompt "You chose to stay!"
+    prompt "You chose to stay at #{total(player)}!"
     "stay"
   end
 end
+# rubocop:enable Metrics/MethodLength
 
-def dealer_turn(player, dealer)
-  answer = nil
+def dealer_turn(_player, dealer)
   loop do
     break if total(dealer) >= 17 || busted?(dealer)
     prompt "Dealer has to hit!"
-    
+
     hit!(dealer)
-    
+
     sleep 1
   end
 
@@ -122,7 +103,7 @@ def dealer_turn(player, dealer)
     sleep 1
     "busted"
   else
-    prompt "Dealer chose to stay!"
+    prompt "Dealer chose to stay at #{total(dealer)}!"
     "stay"
   end
 end
@@ -153,14 +134,21 @@ def display_winner(player, dealer)
     prompt "Alas...you lost."
     sleep 1
   else
-    prompt "Display_winner isn't working :("
+    prompt "It's a tie!"
   end
 
-  prompt "The dealer had #{total(dealer)} and " +
-           "you had #{total(player)}."
+  display_final_cards(player, dealer)
+end
+
+def display_final_cards(player, dealer)
+  prompt "The dealer had " + joinor(current_cards(dealer)) +
+         " for a total of #{total(dealer)}."
+  prompt "You had " + joinor(current_cards(player)) +
+         " for a total of #{total(player)}."
 end
 
 def play_again?
+  puts "-----------------------"
   loop do
     prompt("Do you want to play again? Type Y/Yes or N/No.")
     answer = gets.chomp.downcase
@@ -181,15 +169,14 @@ end
 
 # BEGIN ACTUAL GAME
 
-
-  prompt 'Welcome to 21!'
-  sleep 1
+prompt 'Welcome to 21!'
+sleep 1
 
 loop do
   loop do
     system 'clear'
 
-    initialize_deck(players_cards, dealers_cards)
+    initialize_cards(players_cards, dealers_cards)
     prompt "The dealer will now deal..."
     sleep 1
     display_current_cards(players_cards, dealers_cards)
@@ -205,11 +192,11 @@ loop do
     break if busted?(dealers_cards) || twenty_one?(dealers_cards)
     sleep 1
 
-    break if (calculate_winner(players_cards, dealers_cards) != nil)
+    break if !calculate_winner(players_cards, dealers_cards).nil?
     break if player_turn(players_cards, dealers_cards).include?("stay")
     break if dealer_turn(players_cards, dealers_cards).include?("stay")
   end
-  
+
   calculate_winner(players_cards, dealers_cards)
   display_winner(players_cards, dealers_cards)
   reset_decks(players_cards, dealers_cards)
@@ -218,6 +205,3 @@ loop do
 end
 
 prompt "Thanks for playing 21! Bye now!"
-
-
-
